4d24df1a1f42f68a1192751ddd522ee50e4e6ad0
AropetJ <aropetjoel32@gmail.com>
2023-07-25 15:23:29 +0300

    3-quick_sort.c - cleared the betty checks

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,7 +2,7 @@
 
 void swap(int *a, int *b);
 void quick_sort_recursor(int *array, size_t size, int low, int high);
-int quick_sort_partition(int *array, size_t size,int low, int high);
+int quick_sort_partition(int *array, size_t size, int low, int high);
 
 /**
  * quick_sort - implements a quick sort algorithm
@@ -14,12 +14,12 @@ int quick_sort_partition(int *array, size_t size,int low, int high);
 
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-    {
-        return;
-    }
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
 
-    quick_sort_recursor(array, size, 0, size - 1);
+	quick_sort_recursor(array, size, 0, size - 1);
 }
 
 /**
@@ -42,6 +42,7 @@ void swap(int *a, int *b)
 /**
  * quick_sort_recursor - Repeatetively calls the partion function
  * @array: An array to sort
+ * @size: The length of the array
  * @low: The lower index of the array
  * @high: The higher array index
  *
@@ -50,39 +51,41 @@ void swap(int *a, int *b)
 
 void quick_sort_recursor(int *array, size_t size, int low, int high)
 {
-    if (low < high)
-    {
-        int pivot = quick_sort_partition(array, size, low, high);
-        quick_sort_recursor(array, size, low, pivot - 1);
-        quick_sort_recursor(array, size, pivot + 1, high);
-    }
-    
+	if (low < high)
+	{
+		int pivot = quick_sort_partition(array, size, low, high);
+
+		quick_sort_recursor(array, size, low, pivot - 1);
+		quick_sort_recursor(array, size, pivot + 1, high);
+	}
+
 }
 
 /**
  * quick_sort_partition - Repeatetively calls the partion function
  * @array: An array to sort
+ * @size: The length of the array
  * @low: The lower index of the array
  * @high: The higher array index
  *
  * Return: The index of th pivot
  */
 
-int quick_sort_partition(int *array, size_t size,int low, int high)
+int quick_sort_partition(int *array, size_t size, int low, int high)
 {
-    int pivot = array[high];
-    int j;
-    int i = low;
+	int pivot = array[high];
+	int j;
+	int i = low;
 
-    for (j = low; j < high; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            swap(&array[i], &array[j]);
-            i++;
-        }
-    }
-    swap(&array[i], &array[high]);
-    print_array(array, size);
-    return (i);
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			swap(&array[i], &array[j]);
+			i++;
+		}
+	}
+	swap(&array[i], &array[high]);
+	print_array(array, size);
+	return (i);
 }
