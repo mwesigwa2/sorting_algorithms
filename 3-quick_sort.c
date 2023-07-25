1e9d1505f5bb4ca2385890156edcb52c2c6e6e77
AropetJ <aropetjoel32@gmail.com>
2023-07-25 15:18:06 +0300

    3-quick_sort.c - complete function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,8 @@
 #include "sort.h"
 
 void swap(int *a, int *b);
+void quick_sort_recursor(int *array, size_t size, int low, int high);
+int quick_sort_partition(int *array, size_t size,int low, int high);
 
 /**
  * quick_sort - implements a quick sort algorithm
@@ -12,7 +14,12 @@ void swap(int *a, int *b);
 
 void quick_sort(int *array, size_t size)
 {
+    if (array == NULL || size < 2)
+    {
+        return;
+    }
 
+    quick_sort_recursor(array, size, 0, size - 1);
 }
 
 /**
@@ -30,4 +37,52 @@ void swap(int *a, int *b)
 	temp = *a;
 	*a = *b;
 	*b = temp;
+}
+
+/**
+ * quick_sort_recursor - Repeatetively calls the partion function
+ * @array: An array to sort
+ * @low: The lower index of the array
+ * @high: The higher array index
+ *
+ * Return: No return value
+ */
+
+void quick_sort_recursor(int *array, size_t size, int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = quick_sort_partition(array, size, low, high);
+        quick_sort_recursor(array, size, low, pivot - 1);
+        quick_sort_recursor(array, size, pivot + 1, high);
+    }
+    
+}
+
+/**
+ * quick_sort_partition - Repeatetively calls the partion function
+ * @array: An array to sort
+ * @low: The lower index of the array
+ * @high: The higher array index
+ *
+ * Return: The index of th pivot
+ */
+
+int quick_sort_partition(int *array, size_t size,int low, int high)
+{
+    int pivot = array[high];
+    int j;
+    int i = low;
+
+    for (j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            swap(&array[i], &array[j]);
+            i++;
+        }
+    }
+    swap(&array[i], &array[high]);
+    print_array(array, size);
+    return (i);
 }
