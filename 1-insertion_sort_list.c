6bb3a1d256dc557063f6962b904219dfaad85e66
AropetJ <aropetjoel32@gmail.com>
2023-07-25 19:29:19 +0300

    1-insertion_sort_list.c - first commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,12 +1,37 @@
 #include "sort.h"
 
 /**
- * void insertion_sort_list - implements insertion sort algorithm
- * @list: doubly linked list to be sorted
+ * insertion_sort_list - implements insertion sort algorithm
+ * @list: A pointer to the head of the list to be sorted
  *
- * Return: Nothing
+ * Return: No return value.
  */
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *curr, *key;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (curr = (*list)->next; curr != NULL; curr = curr->next)
+	{
+		key = curr;
+		while (key->prev != NULL && key->n < key->prev->n)
+		{
+			if (key->next != NULL)
+				key->next->prev = key->prev;
+			key->prev->next = key->next;
+			key->next = key->prev;
+			key->prev = key->prev->prev;
+			key->next->prev = key;
+
+			if (key->prev == NULL)
+				*list = key;
+			else
+				key->prev->next = key;
+
+			print_list(*list);
+		}
+	}
 }
