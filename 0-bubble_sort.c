af152ac59418578ce3ee2f2d146608e2da55f728
AropetJ <aropetjoel32@gmail.com>
2023-07-25 11:45:59 +0300

    wrote a separate swap function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,29 +8,45 @@
  * Return: Nothing
  */
 
+void swap(int *a, int *b);
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int tmp, swapp;
+	size_t j;
+	int swapp = 1;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	while (swapp)
 	{
 		swapp = 0;
-
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				print_array(array, size);
+				swap(&array[j], &array[j + 1]);
 				swapp = 1;
+				print_array(array, size);
 			}
 		}
-		if (swapp == 0)
-		{
-			break;
-		}
 	}
+}
+
+/**
+ * swap - Swaps two integers, a and b
+ * @a: Integer to swap
+ * @b: Second integer to sort
+ *
+ * Return: No return value
+ */
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
